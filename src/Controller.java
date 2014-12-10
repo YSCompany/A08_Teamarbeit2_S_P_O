07d4c2cb71426b90a1f2fd3bb13f75be1c26c295
Ahmet Oezsoy <aoezsoy@mail.tgm.ac.at>
2014-12-10 10:56:59 +0100

    Controller erweitert

--- src/Controller.java
+++ src/Controller.java
@@ -2,18 +2,31 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
- * Controller - Die Steuerungsklasse
+ * Controller - Die Steuerungsklasse zum Spiel LightsOut
  * @author Ahmet Oezsoy
  * @version 10.12.2014
  */
 
 public class Controller implements ActionListener {
-
-	public Controller()
-	{
+	
+	
+	/**
+	 * Konstruktor
+	 */
+	public Controller(){
 		
 	}
-	public void actionPerformed(ActionEvent arg0) {
+	/**
+	 * Programmstart
+	 * @param args
+	 */
+	public static void main(String[] args){
+		new Controller();
+	}
+	/**
+	 * ActionListener 
+	 */
+	public void actionPerformed(ActionEvent e) {
 		
 		
 	}
