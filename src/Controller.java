3832f64294eff271a2589dbb7f75f7693b76091c
Yehezkel Sivan <c_sivan@hotmail.com>
2014-12-10 09:13:51 +0100

    leere Klassen

--- src/Controller.java
+++ src/Controller.java
@@ -1,11 +1,18 @@
-/**
- * 
- */
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
- * @author Ahmet Özsoy
- *
+ * Controller - Die Steuerungsklasse
+ * @author Ahmet Oezsoy
+ * @version 10.12.2014
  */
-public class Controller {
+
+public class Controller implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
