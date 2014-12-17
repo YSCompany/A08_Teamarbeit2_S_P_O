d33cb6fe3f017c47503a3c1e955d29207bff5a4e
Ahmet Oezsoy <aoezsoy@mail.tgm.ac.at>
2014-12-17 16:48:27 +0100

    Fertig funktionierende Klasse

--- src/Model.java
+++ src/Model.java
@@ -1,14 +1,17 @@
+import java.io.Closeable;
 import java.util.Random;
+import javax.swing.JOptionPane;
+import com.sun.imageio.stream.StreamCloser.CloseAction;
 
 
 /**
  * Die Model-Klasse zum Spiel LightsOut
  * @author Yehezkel Sivan
- * @version 2014.12.17, v0.8
+ * @version 12.12.2014
  */
 public class Model {
 	private boolean field[][]; //Erstellt ein 2D Array
-	private int height, width;
+	private int height, width; // hoehe und breite
 	
 	/**
 	 * Standard-Konstruktor der Klasse Model
@@ -18,13 +21,13 @@ public class Model {
 	public Model(int height, int width){
 		this.width = width;
 		this.height = height;
-		init();
+		zufaelligeWerte();
 	}
 	
 	/**
 	 * Diese Methode setzt zufällige Werte vom 2D Array auf true oder false
 	 */
-	void init(){
+	public void zufaelligeWerte(){
 		field = new boolean[height][width];
 		Random r = new Random();
 		for(int i=0; i<field.length; i++){
@@ -35,14 +38,37 @@ public class Model {
 	}
 	
 	/**
-	 * Diese Methode setzt alle Werte auf false und bewältigt das Spiel
-	 */
-	void solve(){
-		for(int i=0; i<field.length; i++){
-			for(int e=0; e<field.length; e++){
-				field[i][e] = false;
+	* Diese Methode ueberprueft ob alle Felder den richtigen Zustand haben
+	* @return false wenn ein Feld den Zustand true hat, true wenn alle Felder false sind
+	*/
+	public boolean gewonnenCheck() {
+		for (int i = 0; i < field.length; i++) {
+			for (int j = 0; j < field[i].length; j++) {
+				if (field[i][j] == true) {
+					return false;
+				}
 			}
 		}
+		return true;
+	}
+	
+	/**
+	* Gibt das Feld zurueck
+	* @return the field
+	*/
+	public boolean[][] getField() {
+		return field;
+	}
+	
+	/**
+	 * Diese Methode beendet das Spiel
+	 */
+	public void beenden() {
+		int result = JOptionPane.showConfirmDialog(null,"Möchten Sie das Programm beenden?", "Spiel beenden", JOptionPane.YES_NO_OPTION); 
+		switch(result) { 
+			case JOptionPane.YES_OPTION: 
+			System.exit(0); //Aktion(en) bei Klicken auf den "Ja-Button"
+      } 
 	}
 	
 	/**
@@ -65,7 +91,4 @@ public class Model {
 			field[x][y+1] = !field[x][y+1];
 		}
 	}
-	
-	//checkWin(), getField(), actualStatus()
-	
 }
