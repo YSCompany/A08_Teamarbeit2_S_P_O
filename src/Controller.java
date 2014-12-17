1dc04c5748a3820d265a5f49619bc5fa4e5d3492
Ahmet Oezsoy <aoezsoy@mail.tgm.ac.at>
2014-12-17 16:44:37 +0100

    Fertig funktionierende Klasse

--- src/Controller.java
+++ src/Controller.java
@@ -2,34 +2,66 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
- * Controller - Die Steuerungsklasse zum Spiel LightsOut
- * @author Ahmet Oezsoy
+ * Die Controllerklasse (Steuerungsklasse) zum Spiel LightsOut
+ * @author Oezsoy Ahmet
  * @version 10.12.2014
  */
 
 public class Controller implements ActionListener {
 	
+	private GUI gui; // GUI
+	private Frame frame; // Frame 
+	private Model model; // Model
 	
 	/**
 	 * Konstruktor
 	 */
 	public Controller(){
-		
+		model = new Model(5, 5); // Neues Model erzeugen
+		gui = new GUI(this); // Neues GUI erzeugen
+		frame = new Frame(gui); // Neues Frame erzeugen
 	}
+	
 	/**
-	 * Programmstart
-	 * @param args
+	 * Getter-Methode für die GUI
+	 * @return the gui
 	 */
-	public static void main(String[] args){
-		new Controller();
-		
-	}
+	 public GUI getGui() {
+		 return gui;
+	 }
+	 
 	/**
-	 * ActionListener 
+	 * Getter-Methode für das Model
+	 * @return the model
 	 */
-	public void actionPerformed(ActionEvent e) {
-		
-		
+	 public Model getModel() {
+		 return model;
 	}
-
+	
+	/**
+	 * ActionListener zum reagieren auf das Druecken der Buttons
+	 */
+	 public void actionPerformed(ActionEvent e) {
+		 switch (e.getActionCommand()) {
+		 case "Neues Spiel":
+			 gui.aendern(true);
+			 model.zufaelligeWerte();
+			 gui.neuesSpiel(model.getField());
+			 break;
+		 case "Spiel Beenden":
+			 model.beenden();
+			 break;
+		 default:
+			 int xk = Integer.parseInt(e.getActionCommand()) % 5;
+			 int yk = Integer.parseInt(e.getActionCommand()) / 5;
+			 model.changeField(xk, yk);
+			 gui.druecken(model.getField());
+			 if (model.gewonnenCheck() == true) {
+				 gui.aendern(false);
+				 gui.druecken(model.getField());
+				 gui.gewonnen();
+			 }
+		 }
+	 }
 }
+	
