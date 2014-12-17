0deba1bcf14946eb3de2badca9b1705ae5a0c1fb
Yehezkel Sivan <c_sivan@hotmail.com>
2014-12-17 11:18:34 +0100

    Test-Klasse v1.0 (Final)

---
+++ src/Test.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * Die Test-Klasse zum Spiel LightsOut
+ * @author Yehezkel Sivan
+ * @version 2014.12.17, v1.0
+ */
+public class Test {
+
+	/**
+	 * Test!
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Controller();
+	}
+}
